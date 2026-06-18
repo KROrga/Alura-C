6c9632c031df9454aef807ad9af1cd1de9c5622b
Nicolas <nicolas290509@gmail.com>
2026-06-18 12:16:15 -0300

    Add files via upload
    Pacman

---
+++ FogeFoge (Pacman)/mapa.h
@@ -0,0 +1,38 @@
+#ifndef _MAPA_H_
+#define _MAPA_H_
+
+#define HEROI '@'
+#define FANTASMA '₢'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define PILULA '8'
+
+struct mapa {
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+
+typedef struct mapa MAPA;
+
+struct posicao {
+    int x;
+    int y;
+};
+typedef struct posicao POSICAO;
+
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+void liberamapa(MAPA* m);
+int encontramapa(MAPA* m, POSICAO* p, char c);
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA* m, int x, int y);
+void andanomapa(MAPA* m, int xorigem, int yorigem,
+int xdestino, int ydestino);
+void copiamapa(MAPA* destino, MAPA* origem);
+int podeandar(MAPA* m, char personagem, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+#endif
